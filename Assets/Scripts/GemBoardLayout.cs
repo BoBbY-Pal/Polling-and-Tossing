2306bd56580a792518768816c858889f85af7b70
BoBbY-Pal <officialbobby0001@gmail.com>
2022-07-04 23:26:14 +0530

    Setting up custom board
    -Custom boards for diff levels.

---
+++ Assets/Scripts/GemBoardLayout.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class GemBoardLayout : MonoBehaviour
+{
+    public  LayoutRow[] allRows;
+
+    public Gem[,] GetLayout()
+    {
+        Gem[,] theLayout = new Gem[allRows[0].gemsInRow.Length, allRows.Length];
+
+        for (int y = 0;y < allRows.Length; y++)
+        {
+            for (int x = 0;x < allRows[y].gemsInRow.Length; x++)
+            {
+                if (x < theLayout.GetLength(0))
+                {
+                    if (allRows[y].gemsInRow[x] != null)
+                    {
+                        theLayout[x, allRows.Length - 1 - y] = allRows[y].gemsInRow[x];
+                    }
+                }
+            }
+        }
+
+        return theLayout;
+    }
+}
+
+[System.Serializable]
+public class LayoutRow
+{
+    public Gem[] gemsInRow;
+}
